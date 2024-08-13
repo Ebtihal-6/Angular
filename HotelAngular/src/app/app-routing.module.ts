036ff882d81041621e79b2deaac10288b417461e
Ebtihal-6 <ebtihal66samir@gmail.com>
2024-08-13 12:41:04 +0300

    AddClient

--- HotelAngular/src/app/app-routing.module.ts
+++ HotelAngular/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddClientComponent } from './Components/Clients/add-client/add-client.component';
+import { ViewClientComponent } from './Components/Clients/view-client/view-client.component';
+import { LayoutComponent } from './Components/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LayoutComponent, children: [ // this layout component have header, footer, sidebar,..... implement in it/ and then the routing make children open with this constant layout
+    {path: "AddClient", component:AddClientComponent},
+    {path: "Clients", component: ViewClientComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
